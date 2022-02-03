99aae2ebcfd08090337c3232c2df9d661fd88639
vivianabobbio <93972864+nicologiuggiola@users.noreply.github.com>
2022-02-03 13:08:32 +0100

    ....

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-// const result = Math.sqrt(64);
-// console.log(result);
+const result = Math.sqrt(64);
+console.log(result);
 
-// let pointA = new Point(3, 5);
-// let pointB = new Point(6, 1);
+let pointA = new Point(3, 5);
+let pointB = new Point(6, 1);
 
-// const distance = MathC.calculateDistance(pointA, pointB)
+const distance = MathC.calculateDistance(pointA, pointB)
 
-// console.log(distance);
+console.log(distance);
 
 
 let pointC = new Point(6,3);
 let pointD = new Point(7,3);
 
-const mediaPoint = PuntoMedio.medianPoint(pointC, pointD)
+const mediaPoint = MathC.medianPoint(pointC, pointD);
+const distance2 = MathC.distanceFromOrigin(pointC);
+
+const nearest = MathC.nearestPoint(pointA, pointB, pointC, pointD)
 
 console.log(mediaPoint);
-//
+console.log(distance2);
--- mathC.js
+++ mathC.js
@@ -1,32 +1,52 @@
-// class MathC{
-//     static calculateDistance(pointA, pointB){
-//         const xDif = pointB.x - pointA.x;
-//         const xDif2 = xDif * xDif;
+class MathC {
+    static calculateDistance(pointA, pointB) {
+        const xDif = pointB.x - pointA.x;
+        const xDif2 = xDif * xDif;
 
-//         const yDif = pointB.y - pointA.y;
-//         const yDif2 = yDif * yDif;
+        const yDif = pointB.y - pointA.y;
+        const yDif2 = yDif * yDif;
 
-//         const sumDif2 = xDif2 + yDif2;
-//         const sqrtSumDif2 = Math.sqrt(sumDif2);
-//         return sqrtSumDif2;
-//     }
-// }
-
+        const sumDif2 = xDif2 + yDif2;
+        const sqrtSumDif2 = Math.sqrt(sumDif2);
+        return sqrtSumDif2;
+    }
 
 
 
 
-// funzione "static medianPoint(c,d) => point"   x punto medio e y punto medio  xm=(xc+xd)/2    ym=(yc+yd)/   new point return 
 
 
-class PuntoMedio{
+    // funzione "static medianPoint(c,d) => point"   x punto medio e y punto medio  xm=(xc+xd)/2    ym=(yc+yd)/   new point return 
 
-static medianPoint(c,d){
-const xMed = (c.x + d.x) / 2;
-const yMed = (c.y + d.y) / 2;
 
-const puntoMedio = new Point(xMed, yMed)
-return puntoMedio;
+
+    static medianPoint(c, d) {
+        const xMed = (c.x + d.x) / 2;
+        const yMed = (c.y + d.y) / 2;
+
+        const puntoMedio = new Point(xMed, yMed)
+        return puntoMedio;
+    }
+
+    static distanceFromOrigin(p){
+        const origin = new Point(0,0);
+        const distance = this.calculateDistance(origin , p);
+        return distance;
+
+    }
+    static nearestPoint(startingPoint, ...points){
+        for (const point of points) {
+  
+
+
+
+        }
+    }
+
 }
 
-}
+
+
+
+
+// MathC.nearestPoint (startingPoint, pointA, pointB...) => ritorna quello più vicino allo starting point
